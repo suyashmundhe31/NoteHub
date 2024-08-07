88342d196b2cac052d87b4e356bd86541febc305
Suyash Mundhe <mundhesuyash31@gmail.com>
2024-08-07 15:52:49 +0530

    Index.php

--- Php/Index.php
+++ Php/Index.php
@@ -1,3 +1,12 @@
+<?php 
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    require "../Components/DbConnect.php";
+    
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,24 +39,24 @@
                 </div>
             </section>
             <section id="rightSide">
-                <form action="Login" id="loginForm">
+                <form action="Login.php" id="loginForm" method="post">
                     <div class="LogInForm">
                         <h2>LogIn</h2>
                         <div class="Email">
                             <p>Email: <br>
-                                <input type="email" name="Email" id="Email" placeholder="Enter Your E-Mail" required>
+                                <input type="email" name="email" id="Email" placeholder="Enter Your E-Mail" required>
                             </p>
                         </div>
                         <div class="Password">
                             <p>Password: <br>
-                                <input type="password" name="Password" id="Password" placeholder="Enter Password"
+                                <input type="password" name="password" id="Password" placeholder="Enter Password"
                                     required>
                         </div>
                         <div class="LogIn">
                             <button type="submit" class="Submit">LogIn</button>
                             <div class="signupOption">
                                 <span>Or</span>
-                                <a href="./Signup.html">Signup</a>
+                                <a href="./Signup.php">Signup</a>
                             </div>
                         </div>
                     </div>
