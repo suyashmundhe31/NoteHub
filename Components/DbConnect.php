ab8fdf7ee60e827abe79515d0906e87ac86dd5c3
Suyash Mundhe <mundhesuyash31@gmail.com>
2024-08-08 23:20:53 +0530

    Dbconnect Changes

--- Components/DbConnect.php
+++ Components/DbConnect.php
@@ -2,7 +2,7 @@
     $servername = "Localhost";
     $username = "root";
     $password = "";
-    $database = "Notehub_users";
+    $database = "notehub";
 
     $conn = mysqli_connect($servername, $username, $password , $database);
     if (!$conn) {
